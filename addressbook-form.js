6da0ec29ca94a42814e662d4dca8de9457cbb18e
Romil003 <romilghadge33@gmail.com>
2022-12-11 18:56:03 +0530

    Added Home Page UC4.1

---
+++ addressbook-form.js
@@ -0,0 +1,124 @@
+let contact = {};
+window.addEventListener('DOMContentLoaded',(event) => {
+    const flname = document.querySelector('#flname');
+    const nameError = document.querySelector('.name-error');
+    flname.addEventListener('input',function(){
+        let name = document.querySelector('#flname').value;
+        if (flname.value.length == 0) {
+            nameError.textContent = "";
+            return;
+        }
+        try {
+            (new Contact()).fullName = name;
+            nameError.textContent = "";
+        } catch (e) {
+            nameError.textContent = e;
+        }
+    });
+
+    const phnumber = document.querySelector('#phnumber');
+    const phnumberError = document.querySelector('.phnumber-error');
+    phnumber.addEventListener('input',function(){
+        let phoneNumber = document.querySelector('#phnumber').value;
+        if (phnumber.value.length == 0) {
+            phnumberError.textContent = "";
+            return;
+        }
+        try {
+            (new Contact()).phone = phoneNumber;
+            phnumberError.textContent = "";
+        } catch (e) {
+            phnumberError.textContent = e;
+        }
+    });
+
+    const address = document.querySelector('#address');
+    const addressError = document.querySelector('.address-error');
+    address.addEventListener('input',function(){
+        let address1 = document.querySelector('#address').value;
+        if (address.value.length == 0) {
+            addressError.textContent = "";
+            return;
+        }
+        try {
+            (new Contact()).address = address1;
+            addressError.textContent = "";
+        } catch (e) {
+            addressError.textContent = e;
+        }
+    });
+})
+
+const save = () => {
+    try {
+        let personContact = createContact();
+        createAndUpdateStorage(personContact);
+    } catch(e) {
+        return;
+    }
+}
+
+const createContact = () => {
+    let personContact = new Contact();
+    try {
+        personContact._fullName = getInputValueById('#flname');
+    } catch (e) {
+        setTextValue('.name-error',e);
+    }
+
+    try {
+        personContact._phone = getInputValueById('#phnumber');
+    } catch (e) {
+        setTextValue('.phnumber-error',e);
+    }
+
+    try {
+        personContact._address = getInputValueById('#address');
+    } catch (e) {
+        setTextValue('.address-error',e);
+    }
+
+    personContact._city = getInputValueById('#city');
+    personContact._state = getInputValueById('#state');
+    personContact._zip = getInputValueById('#zipcode');
+    alert(personContact.toString());
+    return personContact;
+}
+
+const getInputValueById = (id) => {
+    let value = document.querySelector(id).value;
+    return value;
+}
+
+const resetForm = () => {
+    setValue('#flname','');
+    setValue('#phnumber','');
+    setValue('#address','');
+    setValue('#city','Select City');
+    setValue('#state','Select State');
+    setValue('#zipcode','');
+}
+
+const setValue = (id,value) => {
+    let element = document.querySelector(id);
+    return element.value = value;
+}
+
+const setTextValue = (id,value) => {
+    let element = document.querySelector(id);
+    element.textContent = value;
+}
+
+function createAndUpdateStorage(personContact) {
+
+    let contactList = JSON.parse(localStorage.getItem("ContactList"));
+
+    if(contactList != undefined){
+        contactList.push(personContact);
+    } else {
+        contactList = [personContact]
+    }
+
+    alert(contactList.toString());
+    localStorage.setItem("ContactList",JSON.stringify(contactList));
+}
---
+++ site_properties.js
@@ -0,0 +1,4 @@
+let site_properties = {
+    home_page : "addressbookhomepage.js",
+    add_emp_payroll_page: "AddressBookFormPage.html"
+}; 
